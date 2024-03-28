513cd4269f431da84fecbbcd44812b71507f9c15
Tuấn Anh <92778429+ntacsharp@users.noreply.github.com>
2024-03-28 16:07:46 +0700

    Initial Create

---
+++ app.js
@@ -0,0 +1,22 @@
+//dependencies
+const express = require('express'); 
+const cors = require('cors');
+require('dotenv').config();
+require('express-async-handler')
+
+
+const app = express(); 
+const PORT = 3000; 
+
+app.get('/', (req, res)=>{ 
+	res.status(200); 
+	res.send("Welcome to root URL of Server"); 
+}); 
+
+app.listen(PORT, (error) =>{ 
+	if(!error) 
+		console.log("Server is Successfully Running, and App is listening on port "+ PORT) 
+	else
+		console.log("Error occurred, server can't start", error); 
+	} 
+); 
