a38f6fcf47370ac37b1e792aea54bf71aa31e70d
Tuấn Anh <92778429+ntacsharp@users.noreply.github.com>
2024-03-29 15:49:43 +0700

    Middlewares

--- app.js
+++ app.js
@@ -2,11 +2,28 @@
 const express = require('express'); 
 const cors = require('cors');
 require('dotenv').config();
-require('express-async-handler')
-
+require('express-async-handler');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express(); 
-const PORT = 3000; 
+const PORT = process.env.PORT || 4000;
+
+const allowCrossDomain = (req, res, next) => {
+    res.header(`Access-Control-Allow-Origin`, `*`);
+    res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
+    res.header(`Access-Control-Allow-Headers`, `Content-Type`);
+    next();
+};
+
+//middlewares
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cors());
+app.use(cookieParser());
+
+
 
 app.get('/', (req, res)=>{ 
 	res.status(200); 
@@ -14,8 +31,9 @@ app.get('/', (req, res)=>{
 }); 
 
 app.listen(PORT, (error) =>{ 
-	if(!error) 
-		console.log("Server is Successfully Running, and App is listening on port "+ PORT) 
+	if(!error){
+        console.log("Server is Successfully Running, and App is listening on port "+ PORT) 
+    }		
 	else
 		console.log("Error occurred, server can't start", error); 
 	} 
