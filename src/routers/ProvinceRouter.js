41671aa66631e1e9b0fab5169921cbd496952b49
Tuấn Anh <92778429+ntacsharp@users.noreply.github.com>
2024-06-12 21:46:11 +0700

    fix

--- src/routers/ProvinceRouter.js
+++ src/routers/ProvinceRouter.js
@@ -4,6 +4,6 @@ const authMiddleware = require('../middlewares/AuthMiddleware');
 
 // router.get("/", authMiddleware, GetProvince);
 router.get("/", ProvinceController.GetProvince);
-router.get("/stop_point/", ProvinceController.GetStopPoint);
+router.post("/stop_point/", ProvinceController.GetStopPoint);
 
 module.exports = router;
